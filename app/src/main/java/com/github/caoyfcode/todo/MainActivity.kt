961a9ee355bf8532ebb0049aea10c2483215995f
caoyf <yyff@mail.ustc.edu.cn>
2023-04-11 21:30:29 +0800

    feat(MainActivity): 添加 UI 内容
    在 `Scaffold` 提供的 content 位置填充内容
    1. 当无 todo 时, 显示 "无待办" 信息
    2. 有 todo 时, 使用 `LazyColumn` 加载未完成的待办、分割线、完成的待办
    其中一条待办使用 `TodoItem` Composable 组件显示

--- app/src/main/java/com/github/caoyfcode/todo/MainActivity.kt
+++ app/src/main/java/com/github/caoyfcode/todo/MainActivity.kt
@@ -3,11 +3,16 @@ package com.github.caoyfcode.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,6 +47,16 @@ fun DefaultPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Screen() {
+    val unchecked_messages = listOf(
+        "Sleep",
+        "Go to School",
+        "Coding"
+    )
+    val checked_messages = listOf(
+        "吃饭",
+        "睡觉",
+        "打游戏",
+    )
     Scaffold(
         modifier = Modifier.padding(10.dp),
         topBar = {
@@ -69,14 +84,74 @@ fun Screen() {
             )
         }
     ) { paddingValues ->
-        Column (
+        Box(
             modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.TopCenter
         ) {
-            Text(text = stringResource(id = R.string.no_todo))
+            if (unchecked_messages.isEmpty()) {
+                Column (
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = stringResource(id = R.string.no_todo))
+                }
+            } else {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .fillMaxHeight()
+                        .padding(vertical = 20.dp), // 前导 20dp
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(20.dp), // 没项相隔 20dp
+                ) {
+                    items(unchecked_messages) {
+                        TodoItem(message = it, false)
+                    }
+                    item {
+                        Divider(
+                            color = Color.LightGray
+                        )
+                    }
+                    items(checked_messages) {
+                        TodoItem(message = it, true)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoItem(message: String, checked: Boolean) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp),
+        shape = RoundedCornerShape(20),
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.inverseSurface)
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 10.dp), // 前导 10dp
+            horizontalArrangement = Arrangement.spacedBy(10.dp), // 中间相隔 10dp
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            if (checked) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(painter = painterResource(id = R.drawable.check), contentDescription = stringResource(
+                        id = R.string.checked_todo
+                    ))
+                }
+            } else {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(painter = painterResource(id = R.drawable.bookmark), contentDescription = stringResource(
+                        id = R.string.unchecked_todo
+                    ))
+                }
+            }
+            Text(text = message)
         }
     }
 }
