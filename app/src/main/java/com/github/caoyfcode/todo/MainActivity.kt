1ee546ae221ff2f88ce3198d36e48d20d3cab155
caoyf <yyff@mail.ustc.edu.cn>
2023-04-11 16:56:26 +0800

    feat(res, MainActivity): 添加顶栏
    1. 使用 Scaffold 脚手架容器, 设置顶栏与内容
    2. 添加 icon.png 并作为 App 的启动图标
    3. 添加 add.xml, list.xml, search.xml 矢量图作为顶栏按钮使用
    4. 添加一些字符串资源

--- app/src/main/java/com/github/caoyfcode/todo/MainActivity.kt
+++ app/src/main/java/com/github/caoyfcode/todo/MainActivity.kt
@@ -3,13 +3,15 @@ package com.github.caoyfcode.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.caoyfcode.todo.ui.theme.TodoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +24,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Screen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TodoTheme {
-        Greeting("Android")
+        Screen()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Screen() {
+    Scaffold(
+        modifier = Modifier.padding(10.dp),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name)) },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(painter = painterResource(id = R.drawable.list), contentDescription = stringResource(
+                            id = R.string.all_groups
+                        ))
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(painter = painterResource(id = R.drawable.search), contentDescription = stringResource(
+                            id = R.string.search
+                        ))
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(painter = painterResource(id = R.drawable.add), contentDescription = stringResource(
+                            id = R.string.add_todo
+                        ))
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column (
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(id = R.string.no_todo))
+        }
     }
 }
